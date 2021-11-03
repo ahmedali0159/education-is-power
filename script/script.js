072aa525227372c520f36120c8cdfa5af2a20003
ahmedali0159 <pirates0159@gmail.com>
2021-11-03 15:13:06 +0600

    added courses three and teachers

--- script/script.js
+++ script/script.js
@@ -7,3 +7,18 @@ document.querySelector("#menu-btn").onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove("active");
 }
+
+let mainVideo = document.querySelector(".main-video");
+
+document.querySelectorAll(".course-3 .box .video video").forEach(vid =>{
+    vid.onclick = () => {
+        let src = vid.getAttribute("src");
+        mainVideo.classList.add("active");
+        mainVideo.querySelector("video").src = src;
+       
+    }
+});
+
+document.querySelector("#close-vid").onclick = () => {
+    mainVideo.classList.remove("active");
+}
